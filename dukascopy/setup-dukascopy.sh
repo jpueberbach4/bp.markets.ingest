2aabc1c2d1d4286a40a7706bef977f8a920974cf
JP Ueberbach <jean.paul.ueberbach@ah.nl>
2025-12-18 14:00:33 +0100

    fix(setup-dukascopy): make multiple runs correct

--- dukascopy/setup-dukascopy.sh
+++ dukascopy/setup-dukascopy.sh
@@ -2,7 +2,8 @@
 
 echo Initializing default Dukascopy configuration...
 
-cp -arf config config.user
+mkdir -p config.user
+cp -r config/* config.user/
 sed 's|config/dukascopy|config.user/dukascopy|g' config.dukascopy-mt4.yaml > config.user.yaml
 
 echo Done.
