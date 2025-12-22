0df9072254720fb82ba40a04790c3ffe1aab8629
JP Ueberbach <jean.paul.ueberbach@ah.nl>
2025-12-22 13:27:39 +0100

    feature(rebuild-aggregate): added rebuild-aggregate.sh script

---
+++ dukascopy/rebuild-aggregate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get exclusive lock
+exec 200>`pwd`/data/locks/run.lock
+flock -x 200  
+echo Deleting data/aggregate and data/resample*...
+rm -rf ./data/resample ./data/aggregate
+echo Rebuilding...
+START_DATE=2005-01-01 NOLOCK=1 ./run.sh
+echo Done.
+# Release lock
+exec 200>&-
