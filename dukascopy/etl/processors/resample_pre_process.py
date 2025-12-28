42cf097441d25d0c19f4bc9a3eba1f9d3a969ead
JP Ueberbach <jtrader25@gmail.com>
2025-12-28 10:36:47 +0100

    feature(resample): more more more comments

--- dukascopy/etl/processors/resample_pre_process.py
+++ dukascopy/etl/processors/resample_pre_process.py
@@ -17,6 +17,9 @@ def get_dst_transitions(start_dt, end_dt):
 def preprocess_origin(tz:str, df: pd.DataFrame, ident, config) -> pd.DataFrame:
     # This is very heavy stuff. If you change this, make sure you know what you are doing
     # This is an attempt to eliminate the line-by-line session determination in resample_batch
+    # I made it myself VERY difficult by not keeping the original UTC times.
+    # Now i need to backconvert. Making this routine extremely complex.
+    # Commenting it line by line in order to not "lose grip"
 
     tz_sg = pytz.timezone(tz)
     tz_ny = pytz.timezone('America/New_York')       # Change this
@@ -29,15 +32,23 @@ def preprocess_origin(tz:str, df: pd.DataFrame, ident, config) -> pd.DataFrame:
 
     first_dt = df.index[0]
     last_dt = df.index[-1]
+
+    # Get the DST transitions in UTC
     transitions = get_dst_transitions(first_dt, last_dt)
+
+    # Determine boundary windows for the DST switches
     boundaries = sorted(list(set([first_dt, last_dt] + transitions)))
     
     df['tz_dt_sg'] = pd.NaT
     df['dst_shift'] = 0
     df['tz_origin'] = "epoch"
 
+    # For each of the DST transition windows
     for i in range(len(boundaries) - 1):
+        # Get the start and end of the window
         start_win, end_win = boundaries[i], boundaries[i+1]
+
+        # Get a mask that marks all dates with this window
         mask = (df.index >= start_win) & (df.index <= end_win)
 
         # If any date did not confirm the mask
@@ -47,7 +58,7 @@ def preprocess_origin(tz:str, df: pd.DataFrame, ident, config) -> pd.DataFrame:
         mid_p = pd.Timestamp(start_win + (end_win - start_win) / 2).to_pydatetime().replace(tzinfo=None)
         is_dst = bool(tz_ny.localize(mid_p).dst())
 
-        # Change this, should be based on offset_shift_map
+        # Change this, should be based on offset_shift_map (if we want to support "exotic" metatrader servers)
         server_tz_str = "Etc/GMT-3" if is_dst else "Etc/GMT-2"
         tz_server_cur = pytz.timezone(server_tz_str)
         server_now_cur = datetime.now(tz_server_cur) 
@@ -56,6 +67,7 @@ def preprocess_origin(tz:str, df: pd.DataFrame, ident, config) -> pd.DataFrame:
         cur_gap = (ref_now.utcoffset().total_seconds() - 
                 server_now_cur.utcoffset().total_seconds()) / 3600
         
+        # For this window, the shift is this amount in hours
         window_shift = int(ref_gap - cur_gap)
 
         # Set the window_shift as a column (for debugging) for this boundary
@@ -94,15 +106,18 @@ def preprocess_origin(tz:str, df: pd.DataFrame, ident, config) -> pd.DataFrame:
         base_h, base_m = map(int, base_origin_str.split(':'))
 
         for r in session.ranges.values():
+            # Get time objects from the range
             st_t = datetime.strptime(r.from_time, "%H:%M").time()
             en_t = datetime.strptime(r.to_time, "%H:%M").time()
             
+            # Get the mask for the range, locate matches in df
             t_mask = (sg_times >= st_t) & (sg_times <= en_t) if st_t <= en_t \
                     else (sg_times >= st_t) | (sg_times <= en_t)
             
+            # Filter the session_mask with the range mask
             m = session_mask & t_mask
 
-            # If we did not have any mask generated, we have no times that apply, continue next range
+            # If we did not have any mask matches, this session range does not apply, move on to the next one
             if not m.any(): continue
 
             # AH! we have matches, now start applying the shift to the origin
