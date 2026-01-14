0d0a5c2fb0ad5e0ad73d429e2fd5923034c88b85
JP Ueberbach <jtrader25@gmail.com>
2026-01-14 17:39:24 +0100

    docs(integration-test2): getting there

--- dukascopy/api/plugins/indicators/stddev.py
+++ dukascopy/api/plugins/indicators/stddev.py
@@ -20,7 +20,8 @@ def meta()->Dict:
     """
     return {
         "author": "Google Gemini",
-        "version": 1.0
+        "version": 1.0,
+        "panel": 1
     }
     
 def warmup_count(options: Dict[str, Any]) -> int:
