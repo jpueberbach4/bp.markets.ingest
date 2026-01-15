a9eeaafa3c04e56294da2681393fa9d1c17a153c
JP Ueberbach <jtrader25@gmail.com>
2026-01-15 21:10:15 +0100

    script(hammer): put in bit normal values if users want to try

--- dukascopy/hammer.sh
+++ dukascopy/hammer.sh
@@ -3,7 +3,7 @@
 # Configuration
 URL="http://localhost:8000/ohlcv/1.1/select/EUR-USD,1m[sma(9):sma(18):sma(32):sma(48):sma(64):sma(100):bbands(20,2.0):sma(200)]/after/2026-01-10%2000:00:59/until/2026-01-12%2000:00:59/output/JSONP?order=asc&limit=1440&callback=__callbackData"
 CONCURRENT_REQUESTS=64
-TOTAL_REQUESTS=500
+TOTAL_REQUESTS=1000
 
 echo "Starting hammer test on $URL"
 echo "Sending $TOTAL_REQUESTS total requests ($CONCURRENT_REQUESTS at a time)..."
