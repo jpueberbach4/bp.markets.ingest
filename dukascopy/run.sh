4b70e0164e5862792663178439dd8f040fadd5c3
JP Ueberbach <jtrader25@gmail.com>
2026-01-26 21:41:21 +0100

    fix(run.sh): added random sleep time to avoid thundering herd

--- dukascopy/run.sh
+++ dukascopy/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+SLEEP_TIME=$(python3 -c "import random; print(random.uniform(5, 31))")
+
+echo "Staggering start: sleeping for ${SLEEP_TIME}s to avoid thundering herd..."
+sleep $SLEEP_TIME
+
 python3 etl/run.py
